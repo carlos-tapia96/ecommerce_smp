59717138aa23596dc1ccfbed891a27e5b0a1e9ac
carlos-tapia96 <johnncarlos7@gmail.com>
2023-12-06 14:01:08 -0400

    ADD admin, utils STORE

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from . models import Product, ShippingAddress, Order, OrderItem, Category
+
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(ShippingAddress)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Category)
+
